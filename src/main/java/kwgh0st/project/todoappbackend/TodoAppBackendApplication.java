8e879550d0c52e8912c73ea802d923f876b20c8b
Karol Programuje <karol341654@gmail.com>
2024-02-29 11:22:12 +0100

    code changes

--- src/main/java/kwgh0st/project/todoappbackend/TodoAppBackendApplication.java
+++ src/main/java/kwgh0st/project/todoappbackend/TodoAppBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,10 +22,11 @@ public class TodoAppBackendApplication {
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings(CorsRegistry registry) {
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
 				registry.addMapping("/**")
 						.allowedMethods("*")
-						.allowedOrigins(env.getProperty("spring.client.url"));
+						.allowedOrigins(env.getProperty("spring.client.url"))
+						.allowedHeaders("*");
 			}
 		};
 	}
