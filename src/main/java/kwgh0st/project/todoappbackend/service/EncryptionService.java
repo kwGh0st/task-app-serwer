d5f54cb0be7f5ee4722345d25c350bd932db8c65
Karol Programuje <karol341654@gmail.com>
2024-03-28 21:32:29 +0100

    Encrypting Todos descriptions - charset fix

--- src/main/java/kwgh0st/project/todoappbackend/service/EncryptionService.java
+++ src/main/java/kwgh0st/project/todoappbackend/service/EncryptionService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.security.SecureRandom;
 import java.util.Base64;
@@ -21,7 +22,7 @@ public class EncryptionService {
             Key key = new SecretKeySpec(KEY, ALGORITHM);
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, key);
-            byte[] encryptedBytes = cipher.doFinal(value.getBytes());
+            byte[] encryptedBytes = cipher.doFinal(value.getBytes(StandardCharsets.UTF_8));
             return Base64.getEncoder().encodeToString(encryptedBytes);
         } catch (Exception ex) {
             logger.debug(ex.getMessage());
@@ -35,7 +36,7 @@ public class EncryptionService {
             Key key = new SecretKeySpec(KEY, ALGORITHM);
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.DECRYPT_MODE, key);
-            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedValue));
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedValue.getBytes(StandardCharsets.UTF_8)));
             return new String(decryptedBytes);
         } catch (Exception ex) {
             logger.debug(ex.getMessage());
